cad063e4a639906a3fe3f5418bdeffc5d5f97346
anthony-okoye <tagboanthonyokoye@gmail.com>
2025-12-19 19:57:57 +0100

    update: cors parameter adjustment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://viva-femini-frontend.vercel.app/'], 
+    origin: [process.env.FRONTEND_URL], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
