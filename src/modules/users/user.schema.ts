9d5d7318568879de10b84106fca012d22d8fe2fe
anthony-okoye <tagboanthonyokoye@gmail.com>
2025-12-19 21:43:27 +0100

    update: implemented schema for user service

---
+++ src/modules/users/user.schema.ts
@@ -0,0 +1,38 @@
+// src/users/schemas/user.schema.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+@Schema({ timestamps: true, collection: 'users' })
+export class User extends Document {
+  @Prop({ 
+    type: String, 
+    unique: true, 
+    index: true, 
+    default: () => `VF-${uuidv4().split('-')[0].toUpperCase()}` 
+  })
+  reference: string;
+
+  @Prop({ required: true, unique: true, lowercase: true, trim: true })
+  email: string;
+
+  @Prop({ required: true, lowercase: true, trim: true })
+  firstname: string;
+
+  @Prop({ required: true, lowercase: true, trim: true })
+  lastname: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: null })
+  profile_url: string;
+
+  @Prop({ default: 28 })
+  avgCycleLength: number;
+
+  @Prop({ default: 5 })
+  avgPeriodLength: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
